2c14fbf33c67741ff6d956a16387024719d914fb
oohara <oohara@172.16.31.96>
2024-04-12 11:40:00 +0900

    examプロジェクトの新規登録

---
+++ src/dao/StudentDao.java
@@ -0,0 +1,32 @@
+package dao;
+
+import java.sql.resultset;
+
+public class StudentDao extends Dao {
+	
+	private String baseSql;
+	
+	public Student get(String no) throws Exception {
+			
+	}
+		
+	private List<Student> postFilter(ResultSet rSet, School school) throws Exception{
+			
+	}
+		
+	public List<Student> filter(School school, int entYear, String classNum, bolean isAttend) throws Exception {
+			
+	}
+		
+	public List<Student> filter(School school, int entYear, bolean isAttend) throws Exception {
+			
+	}
+		
+	public List<Student> filter(School school,, bolean isAttend) throws Exception {
+			
+	}
+		
+	public boolean save(Student student) throws Exception{
+			
+		}
+}
--- src/tool/Action.java
+++ src/tool/Action.java
@@ -5,4 +5,9 @@ import javax.servlet.http.HttpServletResponse;
 
 public abstract class Action {
 	public abstract void execete(HttpServletRequest request, HttpServletResponse response) throws Exception;
+
+	public void Execute(HttpServletRequest reqeust, HttpServletResponse response) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
 }
